fd620ec8afec2540d4e18286b74b844160cdeced
manuvaladares <manuella.valadares@hotmail.com>
2025-11-27 21:24:56 -0300

    feat: adicionar suporte ao plugin VLibras

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,21 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+      </head>
       <body className={`font-sans antialiased ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Toaster richColors />
         <Analytics />
+        <div data-vw="true" className="enabled">
+          <div data-vw-access-button="true" className="active"></div>
+          <div data-vw-plugin-wrapper="true">
+            <div className="vw-plugin-top-wrapper"></div>
+          </div>
+        </div>
+        <script dangerouslySetInnerHTML={{ __html: 'new window.VLibras.Widget("https://vlibras.gov.br/app");' }} />
       </body>
     </html>
   )
